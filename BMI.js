ea1fa7e7bb3c6a3e0e17d2def2d79d9522e70a35
saurabh parasar <saurabhparasar3010@gmail.com>
2021-04-08 00:38:01 +0530

    BMI-CALCULATOR

---
+++ BMI.js
@@ -0,0 +1,25 @@
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBmi = markMass / (markHeight ** 2);
+const johnBmi = johnMass / (johnHeight ** 2);
+
+// console.log(markBmi);
+// console.log(johnBmi);
+
+const markHigherBMI = markBmi > johnBmi;
+// console.log(markHigherBMI);
+
+
+//coding challenge 2 
+
+if (markBmi > johnBmi) {
+    console.log(`Mark's BMI ${markBmi} is higher than John's ${johnBmi}! `)
+} else {
+console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi})!`)
+ };
+
+
+//  coding challenge 
